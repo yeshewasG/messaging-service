9272697e0a780234c5c9fda771a5c4156004f554
yeshewas <yeshewasg12@gmail.com>
2026-03-19 11:29:44 +0300

    compose add redis service name and git reset on action

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,3 +1,13 @@
 import Redis from "ioredis";
-const redis = new Redis();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "redis",
+  port: Number(process.env.REDIS_PORT) || 6379,
+  retryStrategy: (times) => {
+    // reconnect after 2s, 5s, 10s...
+    return Math.min(times * 2000, 10000);
+  },
+  maxRetriesPerRequest: null, // allow infinite retries
+});
+redis.on("connect", () => console.log("✅ Redis connected"));
+redis.on("error", (err) => console.error("Redis error", err));
 export default redis;
